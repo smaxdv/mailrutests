4569c124e5038bafb250d9ac8df176e4d686f38c
smaxdv <smaxdv@gmail.com>
2016-02-28 13:41:35 +1000

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/default
+sudo /etc/init.d/gunicorn restart
+
+
