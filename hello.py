7c0d71af2b1f7d636ec101bc76b09ed20ab59451
smaxdv <smaxdv@gmail.com>
2016-03-07 12:50:57 +1000

    local 0703001

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["Hello!"]
+def app (environ, start_response):
+  status = '200 OK'
+  response_headers = [('Content-type','text/plain')]
+  start_response(status, response_headers)
+  resp = environ['QUERY_STRING'].split("&")
+  resp = [item+"\r\n" for item in resp]
+  return resp
